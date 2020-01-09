7a44f5470bdd84c1e7660cf48955d44a9e4c317a
Dennis Muth <d.muth@gmx.net>
2020-01-09 19:52:46 +0100

    Adds ability to build armhf image on amd64

--- homebot/__init__.py
+++ homebot/__init__.py
@@ -4,7 +4,7 @@ from homebot.assets import AssetManager
 from homebot.flows import Flow
 from homebot.orchestra import Orchestrator
 
-__VERSION__ = '0.1.0'
+__VERSION__ = '0.1.1'
 
 
 __all__ = ['__VERSION__', 'AssetManager', 'Flow', 'Orchestrator']
