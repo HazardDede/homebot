830d8be2f2968955e717efd985bef170e752bc0b
Dennis Muth <d.muth@gmx.net>
2020-01-10 20:47:18 +0100

    Raises homebot version to 0.2.0

--- homebot/__init__.py
+++ homebot/__init__.py
@@ -4,7 +4,8 @@ from homebot.assets import AssetManager
 from homebot.flows import Flow
 from homebot.orchestra import Orchestrator
 
-__VERSION__ = '0.1.1'
+
+__VERSION__ = '0.2.0'
 
 
 __all__ = ['__VERSION__', 'AssetManager', 'Flow', 'Orchestrator']
